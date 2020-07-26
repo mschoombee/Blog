db38cbb41ff71238d4ef32f4ecb64405311f052b
Martin Schoombee <40871893+mschoombee@users.noreply.github.com>
2020-07-26 08:22:50 -0700

    Add files via upload

---
+++ 2020/automating-power-bi-deployments/General - Connect.ps1
@@ -0,0 +1,65 @@
+﻿<# 
+    ** Check the execution policy for the current machine/user, and set it to unrestricted (if required) ** 
+
+    Get-ExecutionPolicy -List
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine #if you want to set it for the machine
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process #if you want to set it for the current process
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser #if you want to set it for current user only
+#>
+
+<#
+    ** Install and import the following Power BI management cmdlets (if required) **
+
+    Install-Module -Name MicrosoftPowerBIMgmt -AllowClobber
+    Import-Module MicrosoftPowerBIMgmt
+#> 
+
+<# 
+    Script Notes
+    -----------------------------------------------------------------------------------------
+
+    ** This script shows the different ways to connect to a Power BI tenant. It isn't meant to 
+    run as a unit, so use if for testing purposes only.
+
+    
+    Technical References
+    -----------------------------------------------------------------------------------------
+
+    Power BI cmdlets
+    https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps 
+
+    Power BI REST APIs
+    https://docs.microsoft.com/en-us/rest/api/power-bi/
+#>
+
+<#-------------------------------------------------------------------------------------------
+
+Interactive Connection - Running this cmdlet will prompt the user to connect
+
+-------------------------------------------------------------------------------------------#>
+
+Connect-PowerBIServiceAccount
+
+
+
+<#-------------------------------------------------------------------------------------------
+
+Automated Connection - Running the code below will connect without user prompts 
+
+Note: 
+** The account you're using will need Power BI administrator permissions. 
+** The method below will not work if MFA is required for the account.
+
+-------------------------------------------------------------------------------------------#>
+
+#Variables 
+$PbiUser = "me@me.com"
+$PbiPassword = "MyPassword"
+
+#Create secure string for password 
+$PbiSecurePassword = ConvertTo-SecureString $PbiPassword -Force -AsPlainText
+$PbiCredential = New-Object Management.Automation.PSCredential($PbiUser, $PbiSecurePassword)
+
+#Connect to the Power BI service
+Connect-PowerBIServiceAccount -Credential $PbiCredential
+
---
+++ 2020/automating-power-bi-deployments/General - PoSh Script Template.ps1
@@ -0,0 +1,68 @@
+﻿<# 
+    ** Check the execution policy for the current machine/user, and set it to unrestricted (if required) ** 
+
+    Get-ExecutionPolicy -List
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine #if you want to set it for the machine
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process #if you want to set it for the current process
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser #if you want to set it for current user only
+#>
+
+<#
+    ** Install and import the following cmdlets (if required) **
+
+    Install-Module -Name <module name> -AllowClobber
+    Import-Module <module name>
+#> 
+
+<# 
+    Script Notes
+    -----------------------------------------------------------------------------------------
+
+    ** This script will... 
+
+    ** Execute this script in the following way in a PowerShell session (run as admin)
+        ex. &'c:\PowerShell\...\General - PoSh Script Template.ps1'
+
+    
+    Technical References
+    -----------------------------------------------------------------------------------------
+
+    <references>
+#>
+
+<#-------------------------------------------------------------------------------------------
+
+Global actions & variables 
+
+-------------------------------------------------------------------------------------------#>
+
+$ErrorActionPreference = "Stop"
+Write-Host "**NOTE: THIS SCRIPT WILL STOP EXECUTING AFTER THE FIRST ERROR**"`r`n -ForegroundColor Yellow 
+
+
+Try 
+{
+
+    <#-------------------------------------------------------------------------------------------
+
+    Section 1
+
+    -------------------------------------------------------------------------------------------#>
+    
+    <code>
+
+}
+
+Catch
+{
+    Write-Host ""`n 
+    Write-Host "An error has occurred!!" -ForegroundColor Red
+    Write-Host "Error Line Number :   $($_.InvocationInfo.ScriptLineNumber)" -ForegroundColor Red
+    Write-Host "Error Command     :   $($_.InvocationInfo.Line.Trim())" -ForegroundColor Red
+    Write-Host "Error Message     :   $($_.Exception.Message)"`n -ForegroundColor Red 
+    Write-Host "Terminating script execution..."`n 
+}
+
+Exit
+
+
