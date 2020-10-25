ea583a55067ae6dad5669176c38689d978f2bd05
Martin Schoombee <40871893+mschoombee@users.noreply.github.com>
2020-10-25 04:10:52 -0700

    Add files via upload

---
+++ 2020/automating-power-bi-deployments/Reports - Update Refresh Schedule.ps1
@@ -0,0 +1,150 @@
+﻿<# 
+    ** Check the execution policy for the current machine/user, and set it to unrestricted (if required) ** 
+
+    Get-ExecutionPolicy -List
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine #if you want to set it for the machine
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process #if you want to set it for the current process
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser #if you want to set it for current user only
+#>
+
+<#
+    ** Install and import the following Power BI management cmdlets (if required) **
+
+    Install-Module -Name MicrosoftPowerBIMgmt -AllowClobber
+    Import-Module MicrosoftPowerBIMgmt
+#> 
+
+<# 
+    Script Notes
+    -----------------------------------------------------------------------------------------
+
+    ** This script will update the refresh schedule (once a day, at the specified time) of a report dataset.
+
+    ** Execute this script in the following way in a PowerShell session (run as admin)
+        ex. &'c:\PowerShell\Power BI\Reports - Update Refresh Schedule.ps1' -WorkspaceName 'MyWorkspace' -ReportName 'MyReportName' -Timezone 'Mountain Standard Time' -RefreshTime '04:30'
+
+    
+    Technical References
+    -----------------------------------------------------------------------------------------
+
+    Power BI cmdlets
+    https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps 
+
+    Power BI REST APIs
+    https://docs.microsoft.com/en-us/rest/api/power-bi/
+#>
+
+<#-------------------------------------------------------------------------------------------
+
+Parameters 
+
+-------------------------------------------------------------------------------------------#>
+
+param 
+(    
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()] 
+    [String]
+    $WorkspaceName, 
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()] 
+    [String]
+    $ReportName, 
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()] 
+    [String]
+    $Timezone,
+     
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()] 
+    [String]
+    $RefreshTime
+)
+
+<#-------------------------------------------------------------------------------------------
+
+Global actions & variables 
+
+-------------------------------------------------------------------------------------------#>
+
+$ErrorActionPreference = "Stop"
+Write-Host "**NOTE: THIS SCRIPT WILL STOP EXECUTING AFTER THE FIRST ERROR**"`r`n -ForegroundColor Yellow 
+
+
+try {
+
+    <#-------------------------------------------------------------------------------------------
+
+    Connect to Power BI 
+
+    -------------------------------------------------------------------------------------------#>
+    
+    #Connect to Power BI tenant
+    Connect-PowerBIServiceAccount | Out-Null 
+
+
+    <#-------------------------------------------------------------------------------------------
+
+    Retrieve workspace and report objects
+
+    -------------------------------------------------------------------------------------------#>
+
+    #Retrieve the workspace
+    $WorkspaceObject = (Get-PowerBIWorkspace -Scope Organization -Name $WorkspaceName)
+
+    #Retrieve the report
+    $PbiReportObject = (Get-PowerBIReport -Workspace $WorkspaceObject -Name $ReportName)
+
+
+    <#-------------------------------------------------------------------------------------------
+
+    Update the dataset refresh schedule 
+
+    -------------------------------------------------------------------------------------------#>
+
+    #API url for the refresh schedule
+    $ApiUrl = "datasets/" + $PbiReportObject.DatasetId + "/refreshSchedule" 
+
+    $ApiRequestBody = @"
+        {
+            "value": {
+                "days": [
+                    "Sunday",
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday"
+                ],
+                "times": [
+                    "$RefreshTime"
+                ],
+                "notifyOption": "MailOnFailure", 
+                "localTimeZoneId": "$Timezone", 
+                "enabled": true
+            }
+        }
+"@ 
+
+
+    #Update refresh schedule
+    Invoke-PowerBIRestMethod -Url $ApiUrl -Method Patch -Body ("$ApiRequestBody")
+
+    Write-Output "Data refresh schedule for report ""$ReportName"" updated successfully to once a day at $RefreshTime in the $Timezone time zone..." `n
+}
+
+catch {
+    Write-Host ""`n 
+    Write-Host "An error has occurred!!" -ForegroundColor Red
+    Write-Host "Error Line Number :   $($_.InvocationInfo.ScriptLineNumber)" -ForegroundColor Red
+    Write-Host "Error Command     :   $($_.InvocationInfo.Line.Trim())" -ForegroundColor Red
+    Write-Host "Error Message     :   $($_.Exception.Message)"`n -ForegroundColor Red 
+    Write-Host "Terminating script execution..."`n 
+}
+
+exit
+
+
